03b1610a552712429e7af961a830acda3fa9c3eb
Subimal Das <er.subimaldas@gmail.com>
2022-07-09 12:25:24 +0530

    Sum of Elements in the array
    serial number 3

---
+++ Array/SumofArray.cpp
@@ -0,0 +1,40 @@
+Approach : 1 Using Loop
+
+#include <iostream>
+
+using namespace std;
+
+int sumArray(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+int main()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    cout << "Sum of all the elments in the array is:" << sumArray(arr, 6) << endl;
+    return 0;
+}
+
+Approach : 2 Using predefined function
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+
+    int n = 5;
+    int arr[] = {1, 2, 3, 4, 5};
+    int sum = 0;
+    cout << "The sum of the elements of the array is: ";
+    // Using predefined function
+    cout << accumulate(arr, arr + n, sum);
+    return 0;
+}
+* /
