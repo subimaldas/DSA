6385a948e36d3102d02c62143bef66d9867d148b
Subimal Das <er.subimaldas@gmail.com>
2022-07-09 13:54:25 +0530

    Linear Search
    Basic Explaination

--- Array/linearsearch.cpp
+++ Array/linearsearch.cpp
@@ -1,3 +1,13 @@
+Approach: 
+1.We declare a variable *key* and starting comparing it every element in the array
+2.IF found then return 1, either return 0.
+    
+Time Complexity: O(n).
+Space Complexity: O(1).
+    
+    
+CODE:
+
 #include <iostream>
 
 using namespace std;
@@ -15,11 +25,13 @@ bool search(int arr[], int size, int key)
 int main()
 {
     int arr1[10] = {56, 4, 6, 77, 81, 0, 21, -32, 9, -10};
-    // whether the element is present int hte given array or not??
+    // whether the element is present in the given array or not??
 
     cout << "Enter the key value:" << endl;
     int key;
     cin >> key;
+    
+    //Declaring a variable *found* to store search result
     bool found = search(arr1, 10, key);
     {
         if (found)
