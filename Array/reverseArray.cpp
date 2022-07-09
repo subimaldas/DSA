05b86d557fefa9bece689743b8240b112e579d70
Subimal Das <er.subimaldas@gmail.com>
2022-07-09 14:09:36 +0530

    Reverse an Array
    https://takeuforward.org/data-structure/reverse-a-given-array/
    
    serial number 2

--- Array/reverseArray.cpp
+++ Array/reverseArray.cpp
@@ -1,7 +1,19 @@
+Question: Reverse an arrray
+
+Approach:
+1. We keep two points, *start* at the beginning and *end* at the last.
+2. Start swaping the start and end , and increse start by 1 nad decrease end by 1 until(start<=end) 
+
+Time Complexity: O(n), single-pass involved.
+Space Complexity: O(1) 
+
+
+CODE:
 #include <iostream>
 
 using namespace std;
 
+//Reverse Function
 void reverseArray(int arr[], int n)
 {
     int start = 0;
@@ -13,6 +25,7 @@ void reverseArray(int arr[], int n)
     }
 }
 
+//Print Function
 void printArray(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
