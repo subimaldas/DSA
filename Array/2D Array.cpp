61edac5355f76afeb6db140add9f090994d1608e
Subimal Das <er.subimaldas@gmail.com>
2022-07-31 20:24:17 +0530

    22. 2D Array

--- Array/2D Array.cpp
+++ Array/2D Array.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    // Create 2D array
+    int arr[3][4];
+
+    // taking input(row)
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> arr[i][j];
+            // For column use arr[j][i]
+        }
+    }
+
+    // print
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
